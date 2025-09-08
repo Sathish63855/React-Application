59b4473f49919fb76975343f716a263eab41a870
Sathish63855 <sathish995325@gmail.com>
2025-09-08 22:22:07 +0530

    new changes

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className="App">
       <div className='container'> 
-          <Header></Header>
+          <Header name='sathish'></Header>
       </div>
       <Footer></Footer>
     </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,12 @@
 import React from "react";
 import '../styles.css';
 
-export default function Header() {
+export default function Header(props) {
     return (
         <div className="header">
             <img src="/logo.png" alt="MovieDux" className="logo" />
             <h2 className="app-subtitle">It's Time for popcorn! Find your next movie here.</h2>
+            <h3>Hi {props.name}!</h3>
         </div>
     );
 }
