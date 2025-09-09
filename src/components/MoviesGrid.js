6f958c55a2df041c38b2495d621d4324d0345b67
Sathish63855 <sathish995325@gmail.com>
2025-09-09 22:30:58 +0530

    Added MovieslIst and Searchbar

--- src/components/MoviesGrid.js
+++ src/components/MoviesGrid.js
@@ -1,8 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import '../styles.css';
+import MovieCard from "./MovieCard";
 
 export default function MoviesGrid() {
+
+    const [movies, setMovies] = useState([]);
+    const [searchTerm, setSearchTerm] = useState("");
+
+    useEffect(() => {
+        fetch("movies.json")
+        .then(response => response.json())
+        .then(data => setMovies(data))
+    },[]);
+
+    const handleSearchChange = (e) => {
+        setSearchTerm(e.target.value);
+    }
+
+    const filteredMovies = movies.filter(movie => 
+        movie.title.toLowerCase().includes(searchTerm.toLowerCase()) 
+    );
+
     return (
-        <div></div>
+        <div>
+            <input 
+                className="search-input"
+                type="text" 
+                placeholder="Search movies..."
+                value={searchTerm}
+                onChange={handleSearchChange}/>
+
+            <div className="movies-grid">
+                {filteredMovies.map((movie) => (
+                    <MovieCard movie={movie}></MovieCard>
+                ))}
+            </div>
+        </div>
     );
 }
